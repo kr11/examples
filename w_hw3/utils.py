9647431623bf2fa638784a0ca22854776a6d490e
kr11 <3095717866@qq.com>
2018-05-23 17:07:56 +0800

    拆分test

--- w_hw3/utils.py
+++ w_hw3/utils.py
@@ -1,4 +1,7 @@
 import pickle
+import argparse
+
+import torch
 
 
 def list_save(content, filename, mode='w'):
@@ -36,6 +39,99 @@ def dict_load(filename):
     content = pickle.load(f2)
     f2.close()
     return content
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser(description='PyTorch RNN/LSTM Language Model')
+    parser.add_argument('--data', type=str, default='./data/ptb',
+                        help='location of the data corpus')
+    parser.add_argument('--model', type=str, default='LSTM',
+                        help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU)')
+    parser.add_argument('--emsize', type=int, default=200,
+                        help='size of word embeddings')
+    parser.add_argument('--nhid', type=int, default=200,
+                        help='number of hidden units per layer')
+    parser.add_argument('--nlayers', type=int, default=2,
+                        help='number of layers')
+    parser.add_argument('--lr', type=float, default=20,
+                        help='initial learning rate')
+    parser.add_argument('--clip', type=float, default=0.25,
+                        help='gradient clipping')
+    parser.add_argument('--epochs', type=int, default=40,
+                        help='upper epoch limit')
+    parser.add_argument('--batch_size', type=int, default=20, metavar='N',
+                        help='batch size')
+    parser.add_argument('--bptt', type=int, default=35,
+                        help='sequence length')
+    parser.add_argument('--dropout', type=float, default=0.2,
+                        help='dropout applied to layers (0 = no dropout)')
+    parser.add_argument('--tied', action='store_true',
+                        help='tie the word embedding and softmax weights')
+    parser.add_argument('--seed', type=int, default=1111,
+                        help='random seed')
+    parser.add_argument('--cuda', action='store_true',
+                        help='use CUDA')
+    parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                        help='report interval')
+    parser.add_argument('--save', type=str, default='model_dir',
+                        help='path to save the final model and dictionary')
+    parser.add_argument('--train_size', type=int, default=-1, metavar='N',
+                        help='train size')
+    parser.add_argument('--valid_size', type=int, default=-1, metavar='N',
+                        help='valid size')
+    parser.add_argument('--test_size', type=int, default=-1, metavar='N',
+                        help='test size')
+
+    args = parser.parse_args()
+    return args
+
+
+def check_device(args):
+    if torch.cuda.is_available():
+        if not args.cuda:
+            print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    return torch.device("cuda" if args.cuda else "cpu")
+
+
+def batchify(data, bsz, device):
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = data.size(0) // bsz
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * bsz)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(bsz, -1).t().contiguous()
+    return data.to(device)
+
+
+def repackage_hidden(h):
+    """Wraps hidden states in new Tensors, to detach them from their history."""
+    if isinstance(h, torch.Tensor):
+        return h.detach()
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
+def get_batch(args, source, i):
+    seq_len = min(args.bptt, len(source) - 1 - i)
+    data = source[i:i + seq_len]
+    target = source[i + 1:i + 1 + seq_len].view(-1)
+    return data, target
+
+
+def evaluate(data_source, model, corpus, criterion, args, eval_batch_size):
+    # Turn on evaluation mode which disables dropout.
+    model.eval()
+    total_loss = 0.
+    ntokens = len(corpus.dictionary)
+    hidden = model.init_hidden(eval_batch_size)
+    with torch.no_grad():
+        for i in range(0, data_source.size(0) - 1, args.bptt):
+            data, targets = get_batch(args, data_source, i)
+            output, hidden = model(data, hidden)
+            output_flat = output.view(-1, ntokens)
+            total_loss += len(data) * criterion(output_flat, targets).item()
+            hidden = repackage_hidden(hidden)
+    return total_loss / len(data_source)
 
 
 if __name__ == '__main__':
